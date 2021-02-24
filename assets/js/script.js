89efd530bb9cf57afb556be7b6629cf210a23917
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-02-24 23:16:37 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,72 @@
-const steps = Array.from( document.querySelectorAll("form .step"));
-const nextBtn = document.querySelectorAll("form .next-btn");
-const prevBtn = document.querySelectorAll("form .previous-btn");
-const form = document.querySelector("form");
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
 
-nextBtn.forEach(button => {
-    button.addEventListener("click", (e) => {
-        changeStep("next");
-    })
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
 });
 
-prevBtn.forEach(button => {
-    button.addEventListener("click", ()=> {
-        changeStep("prev")
-    })
-})
-
-form.addEventListener("submit", (e)=> {
-    e.preventDefault();
-    const inputs = [];
-    form.querySelectorAll("input").forEach(input=> {
-        const{name, value} = input;
-        inputs.push({name, value})
-    })
-    console.log(inputs);
-    form.reset();
-    let index = 0;
-    const active = document.querySelector("form .step.active");
-    index = steps.indexOf(active);
-    steps[index].classList.remove("active")
-    steps[0].classList.add("active")
-})
-
-function changeStep(btn){
-    let index = 0;
-    const active = document.querySelector("form .step.active");
-    index = steps.indexOf(active);
-    steps[index].classList.remove("active");
-    if (btn=== "next"){
-        index ++;
-    }
-
-    else if (btn === "prev"){
-        index--;
-    }
-    steps[index].classList.add("active") 
-    //console.log(index)
-}
-
-
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
