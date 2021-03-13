34ffe35db5f7002688835cda3d52ce0683374e18
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-03-13 22:04:34 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,45 +1,31 @@
 <?php
 
-/*connection à la base de données*/
+//login to database
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=leads;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
 if (isset($_POST["submit"])) {
 
+        $req = $bdd -> prepare("INSERT INTO informations ( nom,prenom, email,telephone, 
+        habitation,region,ville ,code_postal) VALUES (:nom,:prenom,:email,
+        :telephone,:habitation,:region,:ville,:code_postal)");
 
-
-/*recuperation des information */
-    $serveur = "localhost";
-    $login = "root";
-    $pas = "";
-        $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
-        $habitation = $_POST["habitation"];
-        $chauffage = $_POST["chauffage"];
-        $telephone = $_POST["telephone"];
-        $email = $_POST["email"];
-        $region = $_POST["region"];
-        $ville = $_POST["ville"];
-        $code_postal = $_POST["code_postal"];
-        
-    // une methode de connection a la base de donnée
-    $connexion = new PDO("mysql:host=$serveur;dbname=leads", $login, $pas);
-    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//la requete d'insertion dans la base j'ai cree une nouvelle table
-    $req = $connexion->prepare("INSERT INTO informations-prospects ( nom,prenom, 
-    email,telephone, habitation, chauffage, region,ville ,code_postal) VALUES 
-    (:nom,:prenom,:email, :telephone, :habitation, :chauffage, :region,:ville,:code_postal)");
-
-// recuperation et insertion dans les parametres
-    $req->bindParam(':nom', $Nom);
-    $req->bindParam(':prenom', $Prenom);
-    $req->bindParam(':email', $email);
-    $req->bindParam(':telephone', $telephone);
-    $req->bindParam(':habitation', $habitation);
-    $req-> bindParam(':chauffage', $chauffage);
-    $req->bindParam(':region', $region);
-    $req->bindParam(':ville', $code_postal);
-    $req->bindParam(':code_postal', $code_postal);
-
-    $req->execute();
-
-    }
+        $req -> execute (array(
+                "nom" => $_POST["nom"],
+                "prenom" => $_POST["prenom"],
+                "habitation" => $_POST["habitation"],
+                "chauffage" => $_POST["chauffage"],
+                "telephone" => $_POST["telephone"],
+                "email" =>$_POST["email"],
+                "region" => $_POST["region"],
+                "ville" => $_POST["ville"],
+                "code_postal" => $_POST["code_postal"]
+                ));
+         }    
 ?>
