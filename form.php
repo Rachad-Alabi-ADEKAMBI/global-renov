2b1ea421c38ba6c54951c74ba0bff2baf40d7f95
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-03-24 23:41:20 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,31 +1,43 @@
 <?php
-
 //login to database
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=leads;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-
 if (isset($_POST["submit"])) {
 
-        $req = $bdd -> prepare("INSERT INTO informations ( nom,prenom, email,telephone, 
-        habitation,region,ville ,code_postal) VALUES (:nom,:prenom,:email,
-        :telephone,:habitation,:region,:ville,:code_postal)");
+        $serveur = "localhost";
+        $login = "root";
+        $pas = "";
 
-        $req -> execute (array(
-                "nom" => $_POST["nom"],
-                "prenom" => $_POST["prenom"],
-                "habitation" => $_POST["habitation"],
-                "chauffage" => $_POST["chauffage"],
-                "telephone" => $_POST["telephone"],
-                "email" =>$_POST["email"],
-                "region" => $_POST["region"],
-                "ville" => $_POST["ville"],
-                "code_postal" => $_POST["code_postal"]
-                ));
+        $Nom = $_POST["nom"];
+        $Prenom = $_POST["prenom"];
+        $habitation = $_POST["habitation"];
+        $chauffage = $_POST["chauffage"];
+        $telephone = $_POST["telephone"];
+        $region = $_POST["region"];
+        $ville = $_POST["ville"];
+        $code_postal = $_POST["code_postal"];
+        $email = $_POST["email"];
+
+        $connexion=new PDO("mysql:host=$serveur;dbname=leads", $login, $pas);
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        $req = $connexion->prepare("INSERT INTO informations
+         ( nom,prenom, email,telephone, habitation, chauffage, region,
+         ville ,code_postal) VALUES (:nom,:prenom,:email,
+         :telephone,:habitation,:chauffage,:region,:ville
+         ,:code_postal)");
+
+$req->bindParam(':nom', $Nom);
+$req->bindParam(':prenom', $Prenom);
+$req->bindParam(':email', $email);
+$req->bindParam(':telephone', $telephone);
+$req->bindParam(':habitation', $habitation);
+$req->bindParam(':chauffage', $chauffage);
+$req->bindParam(':region', $region);
+$req->bindParam(':ville', $ville);
+$req->bindParam(':code_postal', $code_postal);
+
+$req->execute();
+
+//REDIRECTION
+header('Location: thanks.php');
          }    
 ?>
