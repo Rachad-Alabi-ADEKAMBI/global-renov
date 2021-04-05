a248248c380b7872fc0ea3add73ec32a1533c865
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-04-05 22:23:16 +0200

    Add files via upload

---
+++ mude/mentions.php
@@ -0,0 +1,70 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/sass/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <title>GLOBAL RENOV - MENTIONS LEGALES</title>
+</head>
+
+<body>
+    <div class="top">
+       
+    </div>
+
+    <div class="mentions-container container">
+        <h2 class="mentions-title">
+            MENTIONS LEGALES
+        </h2>
+
+        <p class="mentions-text">
+            Statut du propriétaire : Société <br>
+Le Créateur du site est : contact@globalrenov.fr <br>
+Contactez le responsable de la publication : contact@globalrenov.fr <br>
+Le responsable de la publication est une personne morale. <br>
+
+Hébergeur <br>
+
+OVH FRANCE SAS <br>
+Tél. :  09 72 10 10 10<br>
+Email : support@ovh.com <br>
+Site internet : www.ovh.com <br>
+
+Droits d’auteur – copyright <br>
+
+Tous les éléments, notamment les textes, images, photographies, 
+illustrations, sons, musiques, mis en ligne sur notre site sont,
+sauf mentions particulières, la propriété de globalrenov.fr .
+En conséquence et en application des dispositions du Code de la
+propriété intellectuelle, des dispositions législatives et
+réglementaires de tous pays et des conventions internationales, 
+toute représentation et/ou reproduction, intégrale ou partielle, 
+de l’un quelconque des éléments mentionnés ci-dessus, faite sans 
+le consentement préalable et écrit de contact@globalrenov.fr
+ est interdite. </p><br> <br> <br>
+       
+
+ <footer class="footer">
+            <a href="index.php" class="footer__text">
+                Global Renov ©️ 2020  <br> Tous droits réservés
+            </a>
+
+            <div class="footer__bouton">
+                    <p> Déjà des milliers de Français satisfaits grâce au travail de
+                    nos experts RGE. Remplissez ce formulaire ça ne vous
+                    prendra que quelques secondes.</p> <br>
+
+
+                <a href="index.php" class="footer-btn-link">
+                    <button class="footer-btn">
+                        Remplir le formulaire
+                    </button>
+                </a>
+            </div>
+
+            <div class="footer__disclaimer">
+                 <a href="mentions.php">Mentions légales</a>
+            </div>
+        </footer>
+        </div>
+    </body>
